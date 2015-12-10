abd85247f135c9d867eaacd1f9d9549e1d954360
Pierre-nicolas CARRE <pcarre@dump-mini-1.42.fr>
2015-12-10 18:46:47 +0100

    commit 02

--- ex03/ft_hexdump.c
+++ ex03/ft_hexdump.c
@@ -3,28 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_hexdump.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcarre </var/mail/pcarre>                  +#+  +:+       +#+        */
+/*   By: pcarre <pcarre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/10 15:29:08 by pcarre            #+#    #+#             */
-/*   Updated: 2015/12/10 17:30:00 by pcarre           ###   ########.fr       */
+/*   Created: 2015/12/10 18:42:13 by pcarre            #+#    #+#             */
+/*   Updated: 2015/12/10 18:42:21 by pcarre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int		ft_strcmp(char *s1, char *s2)
-{
-	int		i;
-
-	i = 0;
-	while ((s1[i]) && (s2[i]) && (s1[i] == s2[i]))
-		i++;
-	return (s1[i] - s2[i]);
-}
-
+#include "ft_tools.h"
 
 int		main(int argc, char **argv)
 {
@@ -32,7 +18,7 @@ int		main(int argc, char **argv)
 	int		i;
 
 	i = 1;
-	if (ft_strcmp(argc[2], "-c") != 0)
+	if (ft_strcmp(argc[2], "-C") != 0)
 	{
 		while (i++ < argc)
 		{
--- ex03/ft_tools.c
+++ ex03/ft_tools.c
@@ -6,11 +6,47 @@
 /*   By: pcarre <pcarre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 18:04:13 by pcarre            #+#    #+#             */
-/*   Updated: 2015/12/10 18:04:19 by pcarre           ###   ########.fr       */
+/*   Updated: 2015/12/10 18:42:35 by pcarre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr >= ft_strlen(base))
+	{
+		ft_putnbr_base(nbr / ft_strlen(base), base);
+		ft_putnbr_base(nbr % ft_strlen(base), base);
+	}
+	else
+		ft_putchar(base[nbr]);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int		i;
+
+	i = 0;
+	while ((s1[i]) && (s2[i]) && (s1[i] == s2[i]))
+		i++;
+	return (s1[i] - s2[i]);
 }
