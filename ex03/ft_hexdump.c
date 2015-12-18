3f8b6b22692068fb642920f1234f165785852831
Pierre-nicolas CARRE <pcarre@dump-mini-2.42.fr>
2015-12-19 00:36:33 +0100

    commit 09

--- ex03/ft_hexdump.c
+++ ex03/ft_hexdump.c
@@ -6,7 +6,7 @@
 /*   By: pcarre <pcarre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 18:42:13 by pcarre            #+#    #+#             */
-/*   Updated: 2015/12/18 18:47:19 by pcarre           ###   ########.fr       */
+/*   Updated: 2015/12/19 00:32:02 by pcarre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,62 +28,69 @@ void	ft_first_coll(int l)
 		ft_putstr("00");
 	if (l > 16777215 && l < 268435456)
 		ft_putchar('0');
-	ft_putnbr_base(l, BASE_16);
-	ft_putstr("  ");
 }
 
-int		ft_hexdump_c(int fd, int l)
+void	ft_put_ascii_code(char *tmp, int i)
+{
+	while (tmp[i])
+	{
+		if (i == 8)
+			ft_putchar(' ');
+		if (tmp[i] < 16)
+			ft_putchar('0');
+		ft_putnbr_base(tmp[i], BASE_16);
+		ft_putchar(' ');
+		i++;
+	}
+}
+
+void	ft_third_coll(char* tmp)
+{
+	ft_putchar(' ');
+	ft_putchar('|');
+	ft_putstr(tmp);
+	ft_putchar('|');
+	ft_putchar('\n');
+}
+
+int		ft_global_size(char *argv)
+{
+	int		fd;
+	int		size;
+	char	buf;
+
+	fd = open(argv, O_RDONLY);
+	size = 0;
+	while (read(fd, &buf, 1))
+		size++;
+	close(fd);
+	return (size);
+}
+
+int		ft_hexdump_c(int fd, int l, int t)
 {
 	char	*tmp;
 	int		i;
 
 	tmp = (char*)malloc(sizeof(char) * (BUFF_S + 1));
 	tmp[BUFF_S] = '\0';
-	while (read(fd, tmp, BUFF_S) != 0)
+	while ((read(fd, tmp, BUFF_S) != 0) && (l < t - (t % 16)))
 	{
-/*		if (l < 16)
-			ft_putstr("0000000");
-		if (l > 15 && l < 256)
-			ft_putstr("000000");
-		if (l > 255 && l < 4096)
-			ft_putstr("00000");
-		if (l > 4095 && l < 65536)
-			ft_putstr("0000");
-		if (l > 65535 && l < 1048576)
-			ft_putstr("000");
-		if (l > 1048575 && l < 16777216)
-			ft_putstr("00");
-		if (l > 16777215 && l < 268435456)
-			ft_putchar('0');
-		ft_putnbr_base(l, BASE_16);
-		ft_putstr("  ");*/
-		ft_first_coll(l);
-		i = 0;
-		while (tmp[i])
-		{
-			if (i == 8)
-				ft_putchar(' ');
-			if (tmp[i] < 16)
-				ft_putchar('0');
-			ft_putnbr_base(tmp[i], BASE_16);
-			ft_putchar(' ');
-			i++;
-		}
-		i = 0;
-		while (tmp[i])
-		{
-			if (tmp[i] == '\t' || tmp[i] == '\n')
-				tmp[i] = '.';
-			i++;
-		}
-		ft_putchar(' ');
-		ft_putchar('|');
-		ft_putstr(tmp);
-		ft_putchar('|');
-		ft_putchar('\n');
-		l += 16;
+			ft_first_coll(l);
+			ft_putnbr_base(l, BASE_16);
+			ft_putstr("  ");
+			i = 0;
+			ft_put_ascii_code(tmp, i);
+			i = 0;
+			while (tmp[i])
+			{
+				if (tmp[i] == '\t' || tmp[i] == '\n')
+					tmp[i] = '.';
+				i++;
+			}
+			ft_third_coll(tmp);
+			l += 16;
 	}
-	close(fd);
 	return (l);
 }
 
@@ -92,9 +99,11 @@ int		main(int argc, char **argv)
 	int		fd;
 	int		i;
 	int		l;
+	int		t;
 
 	l = 0;
 	i = 1;
+	t = ft_global_size(argv[i]);
 /*	if (ft_strcmp(argv[1], "-C") != 0)
 	{
 		while (i++ < argc)
@@ -113,7 +122,25 @@ int		main(int argc, char **argv)
 		{
 			fd = open(argv[i], O_RDONLY);
 			if (fd >= 0)
-				l = ft_hexdump_c(fd, l);
+			{
+				l = ft_hexdump_c(fd, l, t);
+				close(fd);
+				if (!(argv[i + 1]))
+				{
+					i = 1;
+					l = 0;
+					while (++i < argc)
+					{
+						l += ft_global_size(argv[i]);
+						if (!(argv[i + 1]))
+						{
+							ft_first_coll(l);
+							ft_putnbr_base(l, BASE_16);
+							ft_putchar('\n');
+						}
+					}
+				}
+			}
 			else
 				return (0);
 		}
