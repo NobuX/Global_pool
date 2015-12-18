06aaafebcec38198a82b06f46d5d58e85e2683fc
Pierre-nicolas CARRE <pcarre@dump-mini-4.42.fr>
2015-12-18 18:53:43 +0100

    commit 08

--- ex03/ft_hexdump.c
+++ ex03/ft_hexdump.c
@@ -6,23 +6,42 @@
 /*   By: pcarre <pcarre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 18:42:13 by pcarre            #+#    #+#             */
-/*   Updated: 2015/12/18 17:44:50 by pcarre           ###   ########.fr       */
+/*   Updated: 2015/12/18 18:47:19 by pcarre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tools.h"
 
-void	ft_hexdump_c(int fd)
+void	ft_first_coll(int l)
+{
+	if (l < 16)
+		ft_putstr("0000000");
+	if (l > 15 && l < 256)
+		ft_putstr("000000");
+	if (l > 255 && l < 4096)
+		ft_putstr("00000");
+	if (l > 4095 && l < 65536)
+		ft_putstr("0000");
+	if (l > 65535 && l < 1048576)
+		ft_putstr("000");
+	if (l > 1048575 && l < 16777216)
+		ft_putstr("00");
+	if (l > 16777215 && l < 268435456)
+		ft_putchar('0');
+	ft_putnbr_base(l, BASE_16);
+	ft_putstr("  ");
+}
+
+int		ft_hexdump_c(int fd, int l)
 {
 	char	*tmp;
 	int		i;
-	int		l;
 
-	l = 0;
 	tmp = (char*)malloc(sizeof(char) * (BUFF_S + 1));
+	tmp[BUFF_S] = '\0';
 	while (read(fd, tmp, BUFF_S) != 0)
 	{
-		if (l < 16)
+/*		if (l < 16)
 			ft_putstr("0000000");
 		if (l > 15 && l < 256)
 			ft_putstr("000000");
@@ -37,7 +56,8 @@ void	ft_hexdump_c(int fd)
 		if (l > 16777215 && l < 268435456)
 			ft_putchar('0');
 		ft_putnbr_base(l, BASE_16);
-		ft_putstr("  ");
+		ft_putstr("  ");*/
+		ft_first_coll(l);
 		i = 0;
 		while (tmp[i])
 		{
@@ -59,19 +79,21 @@ void	ft_hexdump_c(int fd)
 		ft_putchar(' ');
 		ft_putchar('|');
 		ft_putstr(tmp);
-		tmp[i] = '\0';
 		ft_putchar('|');
 		ft_putchar('\n');
 		l += 16;
 	}
 	close(fd);
+	return (l);
 }
 
 int		main(int argc, char **argv)
 {
 	int		fd;
 	int		i;
+	int		l;
 
+	l = 0;
 	i = 1;
 /*	if (ft_strcmp(argv[1], "-C") != 0)
 	{
@@ -91,7 +113,7 @@ int		main(int argc, char **argv)
 		{
 			fd = open(argv[i], O_RDONLY);
 			if (fd >= 0)
-				ft_hexdump_c(fd);
+				l = ft_hexdump_c(fd, l);
 			else
 				return (0);
 		}
